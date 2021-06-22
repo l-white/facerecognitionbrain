65c462551dd0e5186393f06f6b14a869b8684e7c
Laura <law7676@outlook.com>
2021-06-22 07:28:04 -0500

    modified app and ImageLinkForm components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/*
       <ImageLinkForm />
+      {/*
       <FaceRecognition />*/}
     </div>
   );
--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -6,9 +6,11 @@ const ImageLinkForm = () => {
       <p className="f3">
         {'This Magic Brain will detect faces in your photos. Give it a try!'}
       </p>
-      <div>
-        <input className="f4 pa2 w-60 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv dib white bg-light-purple">Detect</button>
+      <div className="center">
+        <div className="pa4 br3 shadow-5">
+          <input className="f4 pa2 w-60 center" type="text" />
+          <button className="w-30 grow f4 link ph3 pv dib white bg-light-red">Detect</button>
+        </div>
       </div>
     </div>
   );
