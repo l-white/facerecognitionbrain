2515d447b56c5bfc34deb967079975bc738d1b74
Laura <law7676@outlook.com>
2021-06-22 06:47:43 -0500

    Added logo component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
 
 function App() {
   return (
