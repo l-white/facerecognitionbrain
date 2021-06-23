1305e2f71c4cc8e228f21142a3230218e267717b
Laura <law7676@outlook.com>
2021-06-23 06:13:06 -0500

    modified App.js, ImageLinkForm.js, and index.css

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      {/*
-      <FaceRecognition />*/}
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <ImageLinkForm />
+        {/*
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
