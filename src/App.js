4afbe862e037ea992ada7ec3c21c7dead46a358c
Laura <law7676@outlook.com>
2021-06-23 06:52:39 -0500

    added particles background and Rank component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import Rank from './components/rank/Rank';
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+    /*
+    line_linked: {
+      shadow: {
+        enable: true,
+        blur: 5
+      }
+    }
+    */
+  }
+};
 
 class App extends Component {
   render () {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions} 
+        />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition />*/}
