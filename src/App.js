56c42b33212b6677ed9dcd06b6348c599178323b
Laura <law7676@outlook.com>
2021-06-26 13:54:51 -0500

    added FaceRecognition component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
+import FaceRecognition from './components/faceRecognition/FaceRecognition';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
@@ -10,7 +11,7 @@ import './App.css';
 
 
 const app = new Clarifai.App({
-  apiKey: '174fc346c4e0464ba9c80ea8cb9265aa',
+  apiKey: '174fc346c4e0464ba9c80ea8cb9265aa'
 });
 
 const particlesOptions = {
@@ -30,26 +31,29 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    console.log('click');
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, 
-      "https://samples.clarifai.com/metro-north.jpg").then(
-  function(response) {
-    console.log(response);
-  },
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(
+      Clarifai.FACE_DETECT_MODEL, 
+      this.state.input)
+      .then(
+        function(response) {
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+        },
   function(err) {
     // there was an error
     console.log(err);
   }
-);
-  }
+  );
+}
 
   render () {
     return (
@@ -63,8 +67,9 @@ class App extends Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        {/*
-        <FaceRecognition />*/}
+          <FaceRecognition 
+            imageUrl={this.state.imageUrl}
+          />
       </div>
     );
   }
