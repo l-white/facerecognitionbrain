fe29c735c1a410d9007219e345ffb56d591095ad
Laura <law7676@outlook.com>
2021-08-20 18:01:14 -0500

    modified Register and SignIn components

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -17,8 +17,20 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    console.log(this.state);
-    this.props.onRouteChange('home');
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data === 'successful login'){
+        this.props.onRouteChange('home');
+      }
+    })
   }
   render(){
     const {onRouteChange} = this.props;
@@ -30,11 +42,13 @@ class SignIn extends React.Component {
             <legend className="f2 fw6 ph0 mh0 center">Sign In</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+              <input onChange={this.onEmailChange} 
+              className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+              <input  onChange={this.onPasswordChange} 
+              className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
             </div>
           <div className="">
             <input
